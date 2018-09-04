d07e02d9dfdc46e62f63c36495acc157b9779577
root <matiar.rahman@gmail.com>
2018-09-04 09:57:58 -0400

    ..

--- samuda-testing-build.sh
+++
@@ -1,6 +0,0 @@
-cd /odoo/custom/addons/
-git pull origin 10.0
-sudo /etc/init.d/odoo-server stop
-sudo /etc/init.d/odoo-server stop
-sudo su odoo -s /bin/bash
-/odoo/odoo-server/odoo-bin --config=/etc/odoo-server.conf -d SCCL_LIVE_AWS -u all
